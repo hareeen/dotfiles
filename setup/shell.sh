5780afbe4953d1fd18929384511b3717a8227cc8
hareen <me@hareen.io>
2024-08-15 17:37:40 +0900

    chore: Update shell.sh to install neovim

--- setup/shell.sh
+++ setup/shell.sh
@@ -20,19 +20,23 @@ fi
 if [ "$DIST" = "ubuntu" ] || [ "$DIST" = "debian" ]; then
     $Sudo apt update
     $Sudo apt install -y gpg wget curl git
+
+    # Add repositories
+    $Sudo mkdir -p /etc/apt/keyrings
+    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | $Sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | $Sudo tee /etc/apt/sources.list.d/gierens.list
+    $Sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+
+    $Sudo add-apt-repository ppa:neovim-ppa/stable
+
+    $Sudo apt update
     
     # Install zsh
     $Sudo apt install -y zsh
     $Sudo chsh -s $(which zsh)
 
-    # Install unix tools
-    $Sudo mkdir -p /etc/apt/keyrings
-    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | $Sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | $Sudo tee /etc/apt/sources.list.d/gierens.list
-    $Sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-    $Sudo apt update
-
-    $Sudo apt install -y tmux openssh-server htop neofetch bat fd-find ripgrep hyperfine zoxide fzf eza
+    # Install tools
+    $Sudo apt install -y tmux openssh-server htop neofetch bat fd-find ripgrep hyperfine zoxide fzf eza neovim
 elif [ "$DIST" = "arch" ]; then
     $Sudo pacman -Syu --noconfirm
 
@@ -40,8 +44,8 @@ elif [ "$DIST" = "arch" ]; then
     $Sudo pacman -S --noconfirm zsh
     $Sudo chsh -s $(which zsh)
 
-    # Install unix tools
-    $Sudo pacman -S --nonconfirm wget tmux openssh htop neofetch bat fd ripgrep hyperfine zoxide fzf eza curl git
+    # Install tools
+    $Sudo pacman -S --nonconfirm wget tmux openssh htop neofetch bat fd ripgrep hyperfine zoxide fzf eza curl git neovim
 else 
     echo "Unsupported distribution."
     exit 1
