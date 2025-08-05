45791a1ba263231d6f422835e07682b1c330820d
hareeen <me@hareen.io>
2025-08-05 16:45:50 +0900

    Remove quotes around root home directory path

--- configurations/home/common/me.nix
+++ configurations/home/common/me.nix
@@ -9,7 +9,7 @@ in {
   home.username = me.username;
   home.homeDirectory = lib.mkDefault (
     if me.username == "root"
-    then "/root"
+    then /root
     else if pkgs.stdenv.isDarwin
     then /Users/${me.username}
     else /home/${me.username}
