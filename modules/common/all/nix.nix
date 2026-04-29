3d84ff2eaaac456bf05d7454ace528ff11c92615
Suyoung Hwang <me@hareen.io>
2026-04-29 11:43:33 +0900

    refactor: refactor nix configuration and switch to Lix package manager

--- modules/common/all/nix.nix
+++ modules/common/all/nix.nix
@@ -13,60 +13,70 @@ in {
       allowUnsupportedSystem = true;
       allowUnfree = true;
     };
-    overlays = lib.attrValues self.overlays;
+    overlays =
+      lib.attrValues self.overlays
+      ++ [
+        (final: prev: {
+          direnv = prev.direnv.overrideAttrs (_: {
+            doCheck = false;
+            doInstallCheck = false;
+          });
+        })
+      ];
   };
 
-  nix =
-    if pkgs.stdenv.isLinux
-    then {
-      enable = true;
+  nix = {
+    enable = true;
+    package = pkgs.lixPackageSets.stable.lix;
 
-      package = pkgs.nixVersions.latest;
+    nixPath = ["nixpkgs=${flake.inputs.nixpkgs}"];
+    registry.nixpkgs.flake = flake.inputs.nixpkgs;
 
-      nixPath = ["nixpkgs=${flake.inputs.nixpkgs}"];
-      registry.nixpkgs.flake = flake.inputs.nixpkgs;
-
-      gc =
-        {
-          automatic = true;
-          options = "--delete-older-than 30d";
-        }
-        // (
-          if pkgs.stdenv.isLinux
-          then {
-            dates = "weekly";
-          }
-          else {
-            interval = {
-              Weekday = 0;
-              Hour = 0;
-              Minute = 0;
-            };
-          }
-        );
-
-      optimise = {
+    gc =
+      {
         automatic = true;
-      };
+        options = "--delete-older-than 30d";
+      }
+      // (
+        if pkgs.stdenv.isLinux
+        then {dates = "weekly";}
+        else {
+          interval = {
+            Weekday = 0;
+            Hour = 0;
+            Minute = 0;
+          };
+        }
+      );
 
-      settings = {
-        max-jobs = "auto";
-        experimental-features = "nix-command flakes";
+    optimise.automatic = true;
 
-        extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
+    settings = {
+      max-jobs = "auto";
+      experimental-features = "nix-command flakes";
 
-        flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
-        trusted-users = [
-          "root"
-          (
-            if pkgs.stdenv.isDarwin
-            then flake.config.me.username
-            else "@wheel"
-          )
-        ];
-      };
-    }
-    else {
-      enable = false;
+      extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
+
+      trusted-users = [
+        "root"
+        (
+          if pkgs.stdenv.isDarwin
+          then flake.config.me.username
+          else "@wheel"
+        )
+      ];
+
+      substituters = [
+        "https://cache.nixos.org"
+        "https://cache.lix.systems"
+        "https://nix-community.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
+  };
 }
