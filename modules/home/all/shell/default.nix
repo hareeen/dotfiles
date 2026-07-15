20165b2340deb3c3ab25492f66d15316579a5968
Suyoung Hwang <me@hareen.io>
2026-07-15 19:05:32 +0900

    feat(starship): fetch nerd font preset

--- modules/home/all/shell/default.nix
+++ modules/home/all/shell/default.nix
@@ -1,5 +1,14 @@
-{flake, ...}: let
+{
+  flake,
+  pkgs,
+  ...
+}: let
   inherit (flake.config) opt;
+
+  nerdFontSymbolsPreset = pkgs.fetchurl {
+    url = "https://starship.rs/presets/toml/nerd-font-symbols.toml";
+    hash = "sha256-uEgo0X18vjxhTAteue5ROnNAmLExsP5y5EkV94tqjOw=";
+  };
 in {
   programs = {
     fzf.enable = true;
@@ -18,7 +27,7 @@ in {
       enableZshIntegration = !opt.enableFish;
       enableFishIntegration = opt.enableFish;
 
-      settings = fromTOML (builtins.readFile ./starship.toml);
+      settings = fromTOML (builtins.readFile nerdFontSymbolsPreset);
     };
   };
 }
